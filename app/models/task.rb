322eb13669533220a58a98568e249364ec9a4d62
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2020-08-04 00:09:14 +0900

    add_custom_valid

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,12 @@
 class Task < ApplicationRecord
   validates :name, presence: true
+  validates :name, length: { maximum: 30 }
+  validate :validate_name_not_including_comma
+
+  private
+
+  # &.ボッチ演算子　nilの場合にNoMethodErrorではなくnilを返す、つまり検証を通ることになる
+  def validate_name_not_including_comma
+    errors.add(:name, "にカンマを含めることはできません") if name&.include?(',')
+  end
 end
