99359908bc2b5a0593132eee7641276d14e70719
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2020-08-04 00:16:59 +0900

    try_callback(before_validate)

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,15 @@
 class Task < ApplicationRecord
+  # before_validation :set_nameless_name
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
   validate :validate_name_not_including_comma
 
   private
+
+  # もしもnameがnilか空白の場合に名前なしを代入させるメソッド
+  # def set_nameless_name
+  #   self.name = "名前なし" if name.blank?
+  # end
 
   # &.ボッチ演算子　nilの場合にNoMethodErrorではなくnilを返す、つまり検証を通ることになる
   def validate_name_not_including_comma
