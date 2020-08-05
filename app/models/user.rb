0eb5613769c7d8140bd22c75a35de2b67a28fe26
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2020-08-05 22:01:12 +0900

    create_user_fuc

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # 下記コードの記述でカラムに対応しない属性が2つ追加(password属性、password_confimartion属性)
   # 属性には一時的にデータが置かれるだけ
   has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
 end
