9d4fe6f5d6a338c9676fd8d5fce535ce9778e970
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2020-08-09 22:23:17 +0900

    change_system_rspec_user_let

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -2,18 +2,19 @@ require 'rails_helper'
 
 describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
+    let(:user_a) { FactoryBot.create(:user, name: "ユーザーA", email: 'a@example') }
+    let(:user_b) { FactoryBot.create(:user, name: "ユーザーB", email: 'b@example') }
+
     before do
-      user_a = FactoryBot.create(:user, name: "ユーザーA", email: 'a@example')
       FactoryBot.create(:task, name: '最初のテスト', user: user_a)
+      visit login_path
+      fill_in 'メールアドレス', with: login_user.email
+      fill_in 'パスワード', with: login_user.password
+      click_button 'ログインする'
     end
 
     context 'ユーザーAがログインしている時' do
-      before do
-        visit login_path
-        fill_in 'メールアドレス', with: 'a@example'
-        fill_in 'パスワード', with: 'password'
-        click_button 'ログインする'
-      end
+      let(:login_user) { user_a }
 
       it 'ユーザーAが作成したタスクが表示される' do
         expect(page).to have_content '最初のタスク'
@@ -21,13 +22,7 @@ describe 'タスク管理機能', type: :system do
     end
 
     context 'ユーザーBがログインしている時' do
-      before do
-        user_a = FactoryBot.create(:user, name: "ユーザーB", email: 'b@example')
-        visit login_path
-        fill_in 'メールアドレス', with: 'b@example'
-        fill_in 'パスワード', with: 'password'
-        click_button 'ログインする'
-      end
+      let(:login_user) { user_b }
 
       it 'ユーザーAが作成したタスクが表示されないこと' do
         expect(page).to have_no_content '最初のタスク'
