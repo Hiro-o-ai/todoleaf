bb311f86e18da72a24f33e250e5d3ae4837a9baf
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2020-08-15 14:16:37 +0900

    add_csv_export

--- app/mailers/task_mailer.rb
+++ app/mailers/task_mailer.rb
@@ -1,12 +1,10 @@
 class TaskMailer < ApplicationMailer
-  default from: 'taskleaf@example.com'
-
   def creation_email(task)
     @task = task
-    mail (
+    mail(
       subject: 'タスク作成完了メール',
       to: 'user@example.com',
       from: 'taskleaf@example.com'
-      )
+    )
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,18 @@
 class Task < ApplicationRecord
-  has_one_attached : image
+  def self.csv_attributes
+    ["name", "description", "created_at", "updated_at"]
+  end
+
+  def self.generate_csv
+    CSV.generate(headers: true) do |csv|
+      csv << csv_attributes
+      all.each do |task|
+        csv << csv_attributes.map{ |attr| task.send(attr) }
+      end
+    end
+  end
+
+  has_one_attached :image
 
   def self.rensackable_attributes(auth_object = nil)
     %w[name created_at]
@@ -8,7 +21,7 @@ class Task < ApplicationRecord
   def self.ransackable_associtations(auth_object = nil)
     []
   end
-  
+
   # before_validation :set_nameless_name
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
